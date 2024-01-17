9a925780813a7669f7a2660cc4a5bb932e8b60df
Chika Okafor <ochika6363@gmail.com>
2024-01-17 05:20:45 -0800

    done

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,15 @@
+-- setup_mysql_dev.sql
+
+-- Create database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create user if it doesn't exist and set password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Apply changes
+FLUSH PRIVILEGES;
+
