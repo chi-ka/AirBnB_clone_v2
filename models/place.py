a123dee2720bc0fd34244d982f814f7a870daa35
Chika Okafor <ochika6363@gmail.com>
2024-01-20 10:16:07 -0800

    done

--- models/place.py
+++ models/place.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Table, Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
+
+
+# Association table for the Many-To-Many relationship
+place_amenity = Table('place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+)
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
-
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -20,4 +26,19 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
 
+    # Relationships
     reviews = relationship('Review', backref='place', cascade='all, delete')
+    amenities = relationship('Amenity', secondary=place_amenity, viewonly=False)
+
+    # FileStorage specific
+    @property
+    def amenities(self):
+        """ Getter for amenities """
+        return self.amenity_ids
+
+    @amenities.setter
+    def amenities(self, obj):
+        """ Setter for amenities """
+        if isinstance(obj, Amenity):
+            self.amenity_ids.append(obj.id)
+
