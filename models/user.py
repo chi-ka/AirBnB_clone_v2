33b3b630aaa8802129cd8817ba3b46992d369b91
Chika Okafor <ochika6363@gmail.com>
2024-01-20 09:50:54 -0800

    done

--- models/user.py
+++ models/user.py
@@ -4,6 +4,8 @@
 from models.base_model import BaseModel
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
 
 class User(BaseModel, Base):
     __tablename__ = 'users'
